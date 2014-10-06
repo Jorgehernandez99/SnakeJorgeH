03759e2c5eee9e45c74384ca7a0d0d87f81db2fb
Jorge Hernandez <jorgeh905187@gmail.com>
2014-10-06 12:08:33 -0700

    added variables
    Added new variables for my snake project.

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,2 +1,18 @@
+var snake;
 
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
 
