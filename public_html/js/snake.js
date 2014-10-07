5289c45a9b109f8bcfb62b0117d460879f61376b
Jorge Hernandez <jorgeh905187@gmail.com>
2014-10-07 11:27:16 -0700

    added color
    Added color code for my Snake Project

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -6,6 +6,13 @@ var screenHeight;
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
@@ -13,6 +20,7 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    
+    context.fillStyle = "rgb(155, 250, 226)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
 }
 
