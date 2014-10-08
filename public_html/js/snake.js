3713f6238e788d6a98ffdfe1e9409bc03ca153f1
Jorge Hernandez <jorgeh905187@gmail.com>
2014-10-08 11:24:40 -0700

    added a snake
    Added a snake for my Snake game Website Project

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,8 +1,13 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
 var screenHeight;
+
+gameInitialize();
+gameDraw();
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -20,7 +25,21 @@ function gameLoop() {
 }
 
 function gameDraw() {
-    context.fillStyle = "rgb(155, 250, 226)";
+    context.fillStyle = "rgb(53, 11, 125)";
     context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 15;
+    snakeSize = 20;
+}
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    
 }
 
